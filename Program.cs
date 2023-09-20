15d66e88e35281ce967e5fbf3f6ac86bde8ecd77
olegsheyko <surikat229official@gmail.com>
2023-09-20 13:49:48 +0300

    Final try 1

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 // Задание - часть 1
 double[] arr = randArrFill(30);
-Console.WriteLine("Source array:");
+Console.WriteLine("Source Array:");
 printArr(arr);
 
 Console.WriteLine("Number of elements in specific range: " + findCount(arr, -15, 45));
@@ -29,6 +29,7 @@ void printArr(double[] arr)
     {
         Console.Write(elmt + "  ");
     }
+    Console.WriteLine();
     Console.WriteLine();
 }
 
@@ -80,3 +81,102 @@ void SortArrayByAbsoluteValue(double[] arr)
 
 // Задание - часть 2
 
+int[,] matrix = RandFillMatrix(5, 6);
+
+int N = 7; // Количество элементов для сдвига
+string direction = "right"; // Режим сдвига: "right" или "down"
+
+Console.WriteLine("Source Matrix:");
+PrintMatrix(matrix);
+
+PushElements(matrix, N, direction);
+
+Console.WriteLine("After push operation:");
+PrintMatrix(matrix);
+
+int[,] RandFillMatrix(int a, int b)
+{
+    int[,] matrix = new int[a, b];
+    Random n = new Random();
+    int rows = matrix.GetLength(0);
+    int cols = matrix.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i, j] = n.Next(-50, 50);
+        }
+    }
+
+    return matrix;
+}
+
+void PushElements(int[,] matrix, int N, string direction)
+{
+    int rows = matrix.GetLength(0);
+    int cols = matrix.GetLength(1);
+
+    if (direction == "right")
+    {
+        N %= cols;
+
+        for (int i = 0; i < rows; i++)
+        {
+            int[] temp = new int[cols];
+
+            for (int j = 0; j < cols; j++)
+            {
+                temp[(j + N) % cols] = matrix[i, j];
+            }
+
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[i, j] = temp[j];
+            }
+        }
+    }
+    else if (direction == "down")
+    {
+        N %= rows; 
+
+        for (int i = 0; i < N; i++)
+        {
+            int[] lastRow = new int[cols];
+
+            for (int j = 0; j < cols; j++)
+            {
+                lastRow[j] = matrix[rows - 1, j];
+            }
+
+            for (int j = rows - 1; j > 0; j--)
+            {
+                for (int k = 0; k < cols; k++)
+                {
+                    matrix[j, k] = matrix[j - 1, k];
+                }
+            }
+
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[0, j] = lastRow[j];
+            }
+        }
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int cols = matrix.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            Console.Write(matrix[i, j] + "       ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
